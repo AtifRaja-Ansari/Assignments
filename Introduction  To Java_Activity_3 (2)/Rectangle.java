0f98a41f0bcc49f4a9559cdead152fd84ee208f4
AtifRaja-Ansari <111556392+AtifRaja-Ansari@users.noreply.github.com>
2022-09-30 12:57:10 +0530

    Add files via upload

---
+++ Introduction  To Java_Activity_3 (2)/Rectangle.java
@@ -0,0 +1,9 @@
+package com.cognizant.shapes;
+
+class Rectangle {
+	void CalculateArea(){
+		System.out.println("The Area of the Rectangle is calculate using the formula length * breadth ");
+		
+	}
+
+}
